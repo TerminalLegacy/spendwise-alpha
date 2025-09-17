d0eec5316fbcd3ebad1ce55fdeb677fbd6ffeae0
Karan Mehta <krnmhta@gmail.com>
2025-09-17 17:17:20 -0400

    Attempting to allow xlsx and pdf upload, improved autocategorization

---
+++ .env_check.py
@@ -0,0 +1,9 @@
+﻿import sys, importlib
+print('Python:', sys.executable)
+mods = ['streamlit','pandas','dateutil','plotly.express','requests','wikipedia','fuzzywuzzy']
+for m in mods:
+    try:
+        importlib.import_module(m)
+        print('OK:', m)
+    except Exception as e:
+        print('FAIL:', m, '->', e)
